962bee45efd11fa121326bb74ecfcb6cb062114b
unknown <949leoni@gmail.com>
2026-01-18 08:25:41 -0300

    Make get all category, update category and delete

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -29,4 +29,42 @@ const getAllCategoryController = async (req, res) =>{
         res.status(500).send({success:false,message:'Error into API get All'})
     }
 }
-module.exports = { createCatController,getAllCategoryController }
+//update category
+const updateCategoryController = async (req, res)=>{
+    try {
+        //get data from category
+        const {id} = req.params
+        const {title,imageUrl} = req.body
+        //validations
+        if(!id) res.status(500).send({success:false,message:'Error to get ID category'})
+        //update categories
+        const updateCategory = await categoryModel.findByIdAndUpdate(id,{title,imageUrl},{new:true})
+        if(!updateCategory)res.status(500).send({success:false,message:'Error to update category'})
+        res.status(200).send({success:true,message:'Update Category with success !'})
+
+    } catch (error) {
+        console.log(error)
+        res.status(500).send({success:false,message:'Error into API Update Category'})
+    }
+}
+//delete category   
+const deleteCategoryController = async (req,res)=>{
+    try {
+        //get data from user for delete category
+        const {id} = req.params
+        //validations
+        if(!id) res.status(500).send({success:false,message:'Error into get id category'})
+        // get category with id
+        const category = await categoryModel.findById(id)
+        //validations
+        if(!category) res.status(500).send({success:false,message:'Error to find category by ID '})
+        //delete category
+        await categoryModel.findByIdAndDelete(id)
+        //response data
+        res.status(200).send({success:true,message:'Category delete with successfuly'})
+    } catch (error) {
+        console.log(error)
+        res.status(500).send({success:false,message:'Error into delete API category'})
+    }
+}
+module.exports = { createCatController,getAllCategoryController, updateCategoryController ,deleteCategoryController}
--- routes/categoryRoutes.js
+++ routes/categoryRoutes.js
@@ -1,9 +1,14 @@
 const express = require('express')
-const { createCatController, getAllCategoryController } = require('../controllers/categoryController')
+const { createCatController, getAllCategoryController, updateCategoryController, deleteCategoryController } = require('../controllers/categoryController')
 const router  = express.Router()
 //all routers
 
 //PUT | CRETE CATEGORY
 router.put('/create-category', createCatController)
-router.get('/get-all-category',getAllCategoryController)
+//GET | GET ALL CATEGORIES
+router.get('/get-all-category', getAllCategoryController)
+//POST | UPDATE CATEGORIES
+router.post('/update-category/:id', updateCategoryController)
+//DELETE | DELETE CATEGORY
+router.delete('/delete-category/:id', deleteCategoryController)
 module.exports = router
